2e26c5ad6dc0a2bfc84728196b7f189d880eb2c3
NCiavone <nciavone@oakland.edu>
2021-12-04 16:57:30 -0500

    MySQL update 2

--- index.js
+++ index.js
@@ -12,9 +12,27 @@ Notes:
 
 
 const express = require('express');
+const mysql = require("mysql");
+const ejs = require("ejs");
+
 
 const app = new express();
+// DataBase Config
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Rocknroll1!",
+   // database: "testDB",
+});
 
+// Creating the DB connection
+db.connect((err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(`Successful connected to the DB....`);
+    }
+  });
 
 //Middleware Function init
 //Body Parser!
@@ -26,13 +44,13 @@ app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
 //route <url the client will use and how the server will repons to that url>
-app.get('/',(req,res) => {
+app.get("/",(req,res) => {
     res.render('index');
 });
 
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> {console.log('Server started on PORT NO. ${PORT}')});
+app.listen(PORT, ()=> console.log(`Server is running on ${PORT}`));
 
 
