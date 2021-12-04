e586eeffa0caf59fafed6d98e5798998ad26becb
NCiavone <nciavone@oakland.edu>
2021-12-04 15:44:00 -0500

    MySQL update

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
+/*
+-----------------------------------------------------------------------------
+Name: index.js
+Author: @NCiavone
+Date Created: 12/2/21
+Purpose: The js landing page for the server.
+Notes:
+    -The noted from this come from fullstack.js from Prof Sen and his 
+    recording from Nov. 18th
+-----------------------------------------------------------------------------
+*/
+
+
 const express = require('express');
 
 const app = new express();
 
+
+//Middleware Function init
+//Body Parser!
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); //to parse the HTML form
+
+//Initialize ejs Middleware
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+
 //route <url the client will use and how the server will repons to that url>
 app.get('/',(req,res) => {
-    res.send('Hello from Homework 5');
+    res.render('index');
 });
+
 
 // Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {console.log('Server started on PORT NO. ${PORT}')});
+app.listen(PORT, ()=> {console.log('Server started on PORT NO. ${PORT}')});
+
+
